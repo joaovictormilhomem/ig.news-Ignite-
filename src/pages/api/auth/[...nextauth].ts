a1df455acb00554d98ef8bc8903e6bb1f788911e
João Victor Milhomem <joaovictormilho@gmail.com>
2022-10-15 11:24:37 -0300

    implementeado salvar usuarios no faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,52 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       authorization: {
-        params:{
-          scope:"read:user"
+        params: {
+          scope: "read:user"
         }
       },
-      
     }),
     // ...add more providers here
   ],
-}
-
-export default NextAuth(authOptions)
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+})
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,13 +1,14 @@
 import Stripe from "stripe";
-import { version } from "../../package.json"
+import packageInfo from '../../package.json';
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
+    //@ts-ignore
     apiVersion: "2020-08-27",
     appInfo: {
       name: "ig.news",
-      version
+      version: packageInfo.version
     }
   }
 )
